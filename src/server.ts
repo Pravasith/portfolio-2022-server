43defb022751792f35bac6bdf87578ca68de24c8
Pravasith <pravas@siberia.io>
2023-10-23 21:19:05 -0400

    fix: revert

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-process.env.NODE_ENV === "production" && require("module-alias/register")
+// process.env.NODE_ENV === "production" && 
+require("module-alias/register")
 
 import cors from "cors"
 import express from "express"
